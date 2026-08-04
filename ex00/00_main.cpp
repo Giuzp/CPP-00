ca4990d62c588a0543ab0eca94e0f84cd611d7e2
dcresce <credomgiu005@gmail.com>
2026-08-04 11:57:54 +0200

    megaphone cleanup

--- ex00/00_main.cpp
+++ ex00/00_main.cpp
@@ -1,25 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   00_main.cpp                                        :+:      :+:    :+:   */
+/*   megaphone.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dcresce <dcresce@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/08/04 11:42:03 by dcresce           #+#    #+#             */
-/*   Updated: 2026/08/04 11:45:00 by dcresce          ###   ########.ch       */
+/*   Created: 2026/08/04 11:57:06 by dcresce           #+#    #+#             */
+/*   Updated: 2026/08/04 11:57:11 by dcresce          ###   ########.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "megaphone.hpp"
+# include <algorithm>
+# include <string>
+# include <iostream>
 
-int	main(int argc, char **argv)
-{
+using namespace std;
+
+int	main(int argc, char **argv) {
 	if (argc == 1) {
 		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
 		return (0);
 	}
-	for (int i = 1; i < argc; i++)
-	{
+	for (int i = 1; i < argc; i++) {
 		string str = argv[i];
 		transform(str.begin(), str.end(), str.begin(), ::toupper);
 		cout << str;
