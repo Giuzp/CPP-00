b08397cb1798931767728868f2cedf6da7f122b5
dcresce <dcresce@student.42lausanne.com>
2026-09-08 10:58:41 +0200

    started working on account class

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -5,14 +5,30 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dcresce <dcresce@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/09/03 13:45:32 by dcresce           #+#    #+#             */
-/*   Updated: 2026/09/03 13:46:40 by dcresce          ###   ########.ch       */
+/*   Created: 2026/09/03 13:55:39 by dcresce           #+#    #+#             */
+/*   Updated: 2026/09/03 14:14:34 by dcresce          ###   ########.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 
-//contructeur
-Account::Account(int initial_deposit) {
-	
+//contructor
+Account::Account(int initial_deposit) : _amount(initial_deposit) {}
+
+//destructor
+Account::~Account() {}
+
+//getters
+int	Account::getNbAccounts() {
+	return _nbAccounts;
 }
+int	Account::getTotalAmount() {
+	return _totalAmount;
+}
+int	Account::getNbDeposits() {
+	return _totalNbDeposits;
+}
+int	Account::getNbWithdrawals() {
+	return _totalNbWithdrawals;
+}
+
